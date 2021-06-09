7dd03c6c50c13746ac0172cd1ebef89612e1309b
asheraz048 <bsef18a036@pucit.edu.pk>
2021-06-10 03:10:43 +0500

    nothing changed

---
+++ app/src/main/java/com/hybrid/pract001/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hybrid.pract001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+        };
+}
